a44ceb70c41056e67bb191586dccb58229d18c6f
Rodolfo Gonzalez <rodoart@ciencias.unam.mx>
2021-09-20 17:45:15 -0500

    Change enviroment.yml and commands to run it.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,23 +7,33 @@ CURRENT_DIR = pathlib.Path(".").resolve()
 MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 
-print(f"Creating virtual enviroment...{RESET_ALL}")
-subprocess.call(['conda', 'env', 'create', '--file', 'environment.yml'])
-
-print(f"Starting virtual envirmoment...{RESET_ALL}")
-subprocess.call(['conda', 'activate', '{{ cookiecutter.project_slug }}'])
-
 print(f"Downloading tex template...{RESET_ALL}")
 import requests
-
 url = 'https://raw.githubusercontent.com/rodoart/latex-apaarticle-cls/master/apa_article.cls'
 r = requests.get(url, allow_redirects=True)
 open(CURRENT_DIR.joinpath("latex", "apa_article.cls"), 'wb').write(r.content)
 
+url = 'https://i.ibb.co/C9HY1K6/logo.png'
+r = requests.get(url, allow_redirects=True)
+open(CURRENT_DIR.joinpath("figures", "logo.png"), 'wb').write(r.content)
+
+print(f"Creating .gitignore file...{RESET_ALL}")
 
 file_object = open('.gitignore', 'a')
 file_object.write('\n/data/')
 file_object.close()
+
+print(f"Creating virtual enviroment...{RESET_ALL}")
+
+enviro_path = CURRENT_DIR.joinpath("python", "environment.yml")
+print(f'"{enviro_path}"')
+
+#subprocess.call(['conda', 'env', 'create', '--file', f'"{enviro_path}"'])
+
+os.system(f'conda env create --file "{enviro_path}"')
+
+print(f"Starting virtual envirmoment...{RESET_ALL}")
+os.system('conda activate {{ cookiecutter.project_slug }}')
 
 print(f"{MESSAGE_COLOR}Almost done!")
 print(f"Initializing a git repository...{RESET_ALL}")
